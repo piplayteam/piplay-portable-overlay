e1d0032d89980822500dcdbd199c8c8e21f049fb
Shea <shea.silverman@ucf.edu>
2017-07-01 17:53:05 -0400

    Create moveROMS.py

---
+++ moveROMS.py
@@ -0,0 +1,47 @@
+
+import os
+import shutil
+
+roms = {
+        'amstradcpc',
+        'fba',
+        'gbc',
+        'megadrive',
+        'ngpc',
+        'sg-1000',
+        'arcade',
+        'fds',
+        'genesis',
+        'n64',
+        'pcengine',
+        'snes',
+        'atari2600',
+        'gamegear',
+        'mame-libretro',
+        'neogeo',
+        'psx',
+        'vectrex',
+        'atari7800',
+        'gb',
+        'mame-mame4all',
+        'nes',
+        'sega32x',
+        'zxspectrum',
+        'atarilynx',
+        'gba',
+        'mastersystem',
+        'ngp',
+        'segacd',
+}
+
+src_dir = '/boot/ROMS/'
+dest_dir = '/home/pi/RetroPie/roms/'
+
+for r in roms:
+        src = src_dir + r
+        dest = dest_dir + r
+        src_files = os.listdir(src)
+        for file_name in src_files:
+            full_file_name = os.path.join(src, file_name)
+            if (os.path.isfile(full_file_name)):
+                shutil.copy(full_file_name, dest)
