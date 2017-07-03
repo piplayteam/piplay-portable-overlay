fb6e601a23ab55382aaaa56c728b4fcbfe736b45
Shea <shea.silverman@ucf.edu>
2017-07-02 21:51:14 -0400

    Update moveROMS.py

--- moveROMS.py
+++ moveROMS.py
@@ -33,6 +33,8 @@ roms = {
         'segacd',
 }
 
+
+
 src_dir = '/boot/ROMS/'
 dest_dir = '/home/pi/RetroPie/roms/'
 
@@ -49,3 +51,17 @@ for r in roms:
                 os.unlink(full_file_name)
 
 print "Finished Moving ROMS"
+
+print "Moving BIOS"
+
+src = '/boot/ROMS/BIOS'
+dest = '/home/pi/RetroPie/BIOS'
+
+src_files = os.listdir(src)
+for file_name in src_files:
+    full_file_name = os.path.join(src, file_name)
+    if (os.path.isfile(full_file_name)):
+        shutil.copy(full_file_name, dest)
+        os.unlink(full_file_name)
+        
+print "Finished Moving BIOS" 
